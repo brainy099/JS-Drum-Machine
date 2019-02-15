33bfbdeef13cbe27b0ab05a9f28c8d832dcf24f3
brainy099 <brainy099@gmail.com>
2019-02-16 03:10:50 +0530

    added onclick event support

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   if (!audio) return;
-
   key.classList.add('playing');
   audio.currentTime = 0;
   audio.play();
@@ -13,6 +12,18 @@ function removeTrans(e) {
   e.target.classList.remove('playing');
 }
 
+
 window.addEventListener('keydown', playSound);
+
 const keys =  Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTrans));
+
+
+function playSound2(sample) {
+  const audio2 = document.querySelector(`audio[data-key="${sample}"]`)
+  const key2 = document.querySelector(`div[data-key="${sample}"]`);
+  if (!audio2) return;
+  key2.classList.add('playing');
+  audio2.currentTime = 0;
+  audio2.play();
+}
